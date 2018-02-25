acae09b768e191e79721e2bd23a103db32e58623
Raiden <shinji.jy@gmail.com>
2018-02-26 01:22:13 +0800

    bump version

---
+++ README.md
@@ -0,0 +1,31 @@
+# react-router-prop-types
+
+Runtime type checking for [react-router](https://github.com/ReactTraining/react-router) props
+
+## Installation
+
+```shell
+npm install react-router-prop-types --save
+```
+
+## Usage
+
+```jsx
+import React from 'react';
+import ReactRouterPropTypes from 'react-router-prop-types';
+
+class MyComponent extends React.Component {
+  static propTypes = {
+    // You can chain any of the above with `isRequired` to make sure a warning
+    // is shown if the prop isn't provided.
+    computedMatch: ReactRouterPropTypes.match.isRequired,
+    history: ReactRouterPropTypes.history.isRequired,
+    location: ReactRouterPropTypes.location.isRequired,
+    match: ReactRouterPropTypes.match.isRequired,
+    route: ReactRouterPropTypes.route.isRequired, // for react-router-config
+  }
+  render() {
+    // ...
+  }
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,59 @@
+import * as PropTypes from 'prop-types';
+
+export const location = PropTypes.shape({
+  hash: PropTypes.string.isRequired,
+  key: PropTypes.string, // only in createBrowserHistory and createMemoryHistory
+  pathname: PropTypes.string.isRequired,
+  search: PropTypes.string.isRequired,
+  state: PropTypes.oneOfType([
+    PropTypes.array,
+    PropTypes.bool,
+    PropTypes.number,
+    PropTypes.object,
+    PropTypes.string,
+  ]), // only in createBrowserHistory and createMemoryHistory
+});
+
+export const history = PropTypes.shape({
+  action: PropTypes.oneOf(['PUSH', 'REPLACE', 'POP']).isRequired,
+  block: PropTypes.func.isRequired,
+  canGo: PropTypes.func, // only in createMemoryHistory
+  createHref: PropTypes.func.isRequired,
+  entries: PropTypes.arrayOf(location), // only in createMemoryHistory
+  go: PropTypes.func.isRequired,
+  goBack: PropTypes.func.isRequired,
+  goForward: PropTypes.func.isRequired,
+  index: PropTypes.number, // only in createMemoryHistory
+  length: PropTypes.number,
+  listen: PropTypes.func.isRequired,
+  location: location.isRequired,
+  push: PropTypes.func.isRequired,
+  replace: PropTypes.func.isRequired,
+});
+
+export const match = PropTypes.shape({
+  isExact: PropTypes.bool,
+  params: PropTypes.object.isRequired,
+  path: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+});
+
+const childRoute = PropTypes.shape({
+  path: PropTypes.string.isRequired,
+  exact: PropTypes.bool,
+  component: PropTypes.element.isRequired,
+  // @ts-ignore TS2448, TS2454
+  routes: PropTypes.arrayOf(childRoute),
+});
+
+export const route = PropTypes.shape({
+  component: PropTypes.element,
+  routes: PropTypes.arrayOf(childRoute),
+});
+
+export default {
+  location,
+  history,
+  match,
+  route,
+};
